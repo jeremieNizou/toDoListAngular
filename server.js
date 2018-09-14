33a03bbd3c532f83e3bcb2b25184139bc21bce79
jeremieNizou <jeremie.nizou@gmail.com>
2018-09-14 15:57:56 +0200

    CreationFinal

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/todolistAngular/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-console.log("listening");
+console.log("listening port "+process.env.PORT || 8080);
